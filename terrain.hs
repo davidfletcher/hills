cb41d87019950f1e33a0232f3f1ab7904f0157f2
David Fletcher <david@bubblycloud.com>
2012-11-26 17:23:15 +0000

    moved mesh creation into its own file

--- terrain.hs
+++ terrain.hs
@@ -1,70 +1,14 @@
 module Main where
 
-import Data.Array.Unboxed
-
-import Stl
-import Topo
+import qualified Stl
+import qualified Model
+import qualified Topo
 
 main :: IO ()
 main = do
-  let region = FileRegion { regFirstLine = 4540, regNumLines = 100 }
-  topo <- parseFile region "srtm_36_01.asc"
+  let region = Topo.FileRegion { Topo.regFirstLine = 4540, Topo.regNumLines = 100 }
+  topo <- Topo.parseFile region "srtm_36_01.asc"
   -- writePGM "out.pgm" topo
-  let hs = topoHeights (4540, 1280) (100, 200) topo
-  let stl = onPlatform (120, 120, 120) $ model hs
-  writeFile "out.stl" (toString "topo" stl)
-
-model :: Heights -> Model
-model hs = fromTris (surface hs ++ walls hs ++ base hs)
-
-surface :: Heights -> [Tri]
-surface = concatMap quadToTri .  quads
-
-walls :: Heights -> [Tri]
-walls hs = concatMap wall [nEdge, eEdge, sEdge, wEdge]
-    -- we arrange the orders so they are clockwise
-    -- so the triangles we make will be clockwise
-    where nEdge = [hs ! (miny, x) | x <- [maxx,maxx-1..minx]]
-          sEdge = [hs ! (maxy, x) | x <- [minx..maxx]]
-          eEdge = [hs ! (y, maxx) | y <- [maxy,maxy-1..miny]]
-          wEdge = [hs ! (y, minx) | y <- [miny..maxy]]
-          ((miny, minx), (maxy, maxx)) = bounds hs
-
-base :: Heights -> [Tri]
-base hs = quadToTri ( withZ baseZ nw,
-                      withZ baseZ sw,
-                      withZ baseZ se,
-                      withZ baseZ ne )
-    where nw = hs ! (miny, minx)
-          ne = hs ! (miny, maxx)
-          sw = hs ! (maxy, minx)
-          se = hs ! (maxy, maxx)
-          ((miny, minx), (maxy, maxx)) = bounds hs
-
-baseZ :: Double
-baseZ = -100 -- TODO
-
-wall :: [R3] -> [Tri]
-wall edge = concatMap quadToTri $ zipWith wallQuad edge (tail edge)
-    where wallQuad top0 top1 = (top0, top1, bottom1, bottom0)
-              where bottom0 = withZ baseZ top0
-                    bottom1 = withZ baseZ top1
-
-withZ :: Double -> R3 -> R3
-withZ z (x, y, _) = (x, y, z)
-
--- not necessarily planar
-type Quad = (R3, R3, R3, R3)
-
-quads :: Heights -> [Quad]
-quads hs = map quad neCoords
-    where neCoords = [(y, x) | y <- [miny..maxy-1], x <- [minx..maxx-1]]
-          ((miny, minx), (maxy, maxx)) = bounds hs
-          quad (y, x) = ( (pt x y),
-                          (pt x (y+1)),
-                          (pt (x+1) (y+1)),
-                          (pt (x+1) y) )
-          pt x y = hs ! (y, x)
-
-quadToTri :: Quad -> [Tri]
-quadToTri (a, b, c, d) = [(a, b, c), (c, d, a)]
+  let hs = Topo.topoHeights (4540, 1280) (100, 200) topo
+  let stl = Model.model hs
+  writeFile "out.stl" (Stl.toString "topo" stl)
