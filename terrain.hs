3a7097002cb0462837fa337c4b5edac0791d3bf1
David Fletcher <david@bubblycloud.com>
2012-11-28 21:06:24 +0000

    size option

--- terrain.hs
+++ terrain.hs
@@ -30,8 +30,9 @@ parseArgs argv = do
 run :: Opts -> IO ()
 run opts = do
   let centre = optCentre opts
+  let size = optSize opts
   let file = head $ optInFiles opts -- TODO
-  let area = fromJust $ Topo.areaFromCentreAndSize centre (100, 200)
+  let area = fromJust $ Topo.areaFromCentreAndSize centre size
   topo <- Parse.readAsc area file
   -- writePGM "out.pgm" topo
   let hs = Topo.topoHeights area topo
@@ -39,15 +40,18 @@ run opts = do
   writeFile "out.stl" (Stl.toString "topo" stl)
 
 data Opts = Opts { optCentre :: LatLong
+                 , optSize :: (Int, Int)
                  , optInFiles :: [String] }
 
 validateOpts :: ParsedOpts -> Either String Opts
 validateOpts opts = do
   c <- poptCentre opts
+  sz <- poptSize opts
   let files = poptInFiles opts
   case files of [] -> Left "no input files listed"
                 _ -> Right ()
   return $ Opts { optCentre = c
+                , optSize = sz
                 , optInFiles = files }
 
 usageHeader :: String
@@ -55,11 +59,15 @@ usageHeader  = "usage: terrain [options] -c LAT,LONG"
 
 data ParsedOpts = ParsedOpts
     { poptCentre :: Either String LatLong
+    , poptSize :: Either String (Int, Int)
     , poptInFiles :: [String]
     } deriving Show
 
 setCentre :: Either String LatLong -> ParsedOpts -> ParsedOpts
 setCentre v opts = opts { poptCentre = v }
+
+setSize :: Either String (Int, Int) -> ParsedOpts -> ParsedOpts
+setSize v opts = opts { poptSize = v }
 
 addInFile :: String -> ParsedOpts -> ParsedOpts
 addInFile v opts = opts { poptInFiles = v : poptInFiles opts }
@@ -68,6 +76,7 @@ defaultOpts :: ParsedOpts
 defaultOpts =
     ParsedOpts
     { poptCentre = Left "no center point supplied"
+    , poptSize = Right (100, 200)
     , poptInFiles = []
     }
 
@@ -76,6 +85,9 @@ options = [
   Option ['c'] ["center"]
              (ReqArg (Endo . setCentre . parseLatLongOpt) "LAT,LONG")
              "center point",
+  Option ['s'] ["size"]
+             (ReqArg (Endo . setSize . parseSizeOpt) "LATSAMPSxLONGSAMPS")
+             "size in samples",
   Option ['i'] ["infile"]
              (ReqArg (Endo . addInFile) "FILENAME")
              "input data file"
@@ -85,3 +97,12 @@ parseLatLongOpt :: String -> Either String LatLong
 parseLatLongOpt s = case parseLatLong s of
                       Nothing -> Left ("bad lat/long '" ++ s ++ "'")
                       Just x -> Right x
+
+parseSizeOpt :: String -> Either String (Int, Int)
+parseSizeOpt s = case (reads latPart, reads longPart) of
+                   ( [(lat, [])], [(long, [])] ) -> Right (lat, long)
+                   _ -> Left $ "bad size '" ++ s ++ "'"
+    where
+      (latPart, rest) = break (== 'x') s
+      longPart = case rest of [] -> []
+                              (_:xs) -> xs
