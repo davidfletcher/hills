da5a94d908004c9be8d861e768a325bfc6efa8b9
David Fletcher <david@bubblycloud.com>
2013-02-25 13:07:16 +0000

    change to optparse-applicative

--- terrain.hs
+++ terrain.hs
@@ -10,26 +10,13 @@ import qualified Topo
 
 import Data.Maybe
 import Data.Monoid
-import System.Console.GetOpt
-import System.Environment
+import Options.Applicative
 import System.IO
 
 main :: IO ()
 main = do
-  argv <- getArgs
-  case parseArgs argv of
-    Left err -> putStr ("error: " ++ err ++ "\n\n"
-                        ++ usageInfo usageHeader options)
-    Right opts -> run opts
-
-parseArgs :: [String] -> Either String Opts
-parseArgs argv = do
-  opts <- case getOpt Permute options argv of
-            (es, infiles, []) ->
-                Right $ setInFiles infiles . appEndo (mconcat es) $ defaultOpts
-            (_, _, errs) ->
-                Left $ concat errs
-  validateOpts opts
+  conf <- execParser optInfo
+  run conf
 
 run :: Opts -> IO ()
 run opts = do
@@ -55,69 +42,39 @@ makeStl opts samps = Stl.toString "topo" stl
   where baseAlt = optBaseAlt opts
         stl = Model.model baseAlt samps
 
-data Opts = Opts { optCentre :: LatLong
-                 , optSize :: (Int, Int)
-                 , optInFiles :: [String]
-                 , optBaseAlt :: Double }
-
-validateOpts :: ParsedOpts -> Either String Opts
-validateOpts opts = do
-  c <- poptCentre opts
-  sz <- poptSize opts
-  baseAlt <- poptBaseAlt opts
-  let files = poptInFiles opts
-  return Opts { optCentre = c
-              , optSize = sz
-              , optInFiles = files
-              , optBaseAlt = baseAlt }
-
-usageHeader :: String
-usageHeader  = "usage: terrain [options] -c LAT,LONG INFILES..."
-
-data ParsedOpts = ParsedOpts
-    { poptCentre :: Either String LatLong
-    , poptSize :: Either String (Int, Int)
-    , poptInFiles :: [String]
-    , poptBaseAlt :: Either String Double
+data Opts = Opts
+    { optCentre :: LatLong
+    , optSize :: (Int, Int)
+    , optBaseAlt :: Double
+    , optInFiles :: [String]
     } deriving Show
 
-setCentre :: Either String LatLong -> ParsedOpts -> ParsedOpts
-setCentre v opts = opts { poptCentre = v }
+optInfo :: ParserInfo Opts
+optInfo = info (helper <*> optParser) fullDesc
 
-setSize :: Either String (Int, Int) -> ParsedOpts -> ParsedOpts
-setSize v opts = opts { poptSize = v }
+optParser :: Parser Opts
+optParser =
+    Opts
+    <$> nullOption ( short 'c'
+                     <> long "center"
+                     <> reader parseLatLongOpt
+                     <> metavar "LAT,LONG"
+                     <> help "center point" )
+    <*> option ( short 's'
+                 <> long "size"
+                 <> value (300, 600) -- TODO might break with non-multiples just now
+                 <> metavar "LATSAMPSxLONGSAMPS"
+                 <> help "size in samples" )
+    <*> option ( short 'b'
+                 <> long "base-altitude"
+                 <> value ((-100) :: Double)
+                 <> metavar "METERS"
+                 <> help "base altitude" )
+    <*> arguments Just ( metavar "INPUT FILES" )
 
-setBaseAlt :: Either String Double -> ParsedOpts -> ParsedOpts
-setBaseAlt v opts = opts { poptBaseAlt = v }
-
-setInFiles :: [String] -> ParsedOpts -> ParsedOpts
-setInFiles v opts = opts { poptInFiles = v }
-
-defaultOpts :: ParsedOpts
-defaultOpts =
-    ParsedOpts
-    { poptCentre = Left "no center point supplied"
-    , poptSize = Right (300, 600) -- TODO might break with non-multiples just now
-    , poptInFiles = []
-    , poptBaseAlt = Right (-100)
-    }
-
-options :: [OptDescr (Endo ParsedOpts)]
-options = [
-  Option ['c'] ["center"]
-             (ReqArg (Endo . setCentre . parseLatLongOpt) "LAT,LONG")
-             "center point",
-  Option ['s'] ["size"]
-             (ReqArg (Endo . setSize . parseSizeOpt) "LATSAMPSxLONGSAMPS")
-             "size in samples",
-  Option ['b'] ["base-altitude"]
-             (ReqArg (Endo . setBaseAlt . parseDouble) "METRES")
-             "base altitude"
-  ]
-
-parseLatLongOpt :: String -> Either String LatLong
+parseLatLongOpt :: String -> Either ParseError LatLong
 parseLatLongOpt s = case parseLatLong s of
-                      Nothing -> Left ("bad lat/long '" ++ s ++ "'")
+                      Nothing -> Left $ ErrorMsg ("bad lat/long '" ++ s ++ "'")
                       Just x -> Right x
 
 parseSizeOpt :: String -> Either String (Int, Int)
@@ -128,8 +85,3 @@ parseSizeOpt s = case (reads latPart, reads longPart) of
       (latPart, rest) = break (== 'x') s
       longPart = case rest of [] -> []
                               (_:xs) -> xs
-
-parseDouble :: String -> Either String Double
-parseDouble s = case reads s of
-                  [(v, [])] -> Right v
-                  _ -> Left $ "bad number '" ++ s ++ "'"
