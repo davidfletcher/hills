e499bf5acbdb1f0f22f986ef38984c016b07afaa
David Fletcher <david@bubblycloud.com>
2014-09-26 18:24:04 +0100

    improve readme, add test script

---
+++ test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e -u
+
+grep '^    hills' README | cut -d' ' -f6- | sort -u | \
+    while read xxx; do
+        echo dist/build/hills/hills -i $HOME/share/hills $xxx
+        dist/build/hills/hills -i $HOME/share/hills $xxx
+    done
