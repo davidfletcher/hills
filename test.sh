bd94a44e2d614497ada7fa93be3a2a33856178e4
David Fletcher <david@bubblycloud.com>
2014-09-29 12:56:02 +0100

    update some refs to the readme

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 
 set -e -u
 
-grep '^    hills' README | cut -d' ' -f6- | sort -u | \
+grep '^    hills' README.md | cut -d' ' -f6- | sort -u | \
     while read xxx; do
         echo dist/build/hills/hills -i $HOME/share/hills $xxx
         dist/build/hills/hills -i $HOME/share/hills $xxx
