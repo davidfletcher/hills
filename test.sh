5e1248e3539ce812620a0b02aeca47a9867c2039
David Fletcher <david@bubblycloud.com>
2015-09-25 14:32:20 +0100

    build with rts

--- test.sh
+++ test.sh
@@ -4,6 +4,6 @@ set -e -u
 
 grep '^    hills' README.md | cut -d' ' -f6- | sort -u | \
     while read xxx; do
-        echo dist/build/hills/hills -i $HOME/share/hills $xxx
-        dist/build/hills/hills -i $HOME/share/hills $xxx
+        echo dist/build/hills/hills -i $HOME/share/hills $xxx +RTS -K1K
+        dist/build/hills/hills -i $HOME/share/hills $xxx +RTS -K1K
     done
